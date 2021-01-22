cf7a61687a8c89b687a738aac5613e687de1863b
Seikilos <Seikilos@users.noreply.github.com>
2021-01-22 19:12:16 +0100

    Basic design for accessor defined

---
+++ LargeTextFileIndexerTests/LargeFileIndexerAccessTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Seikilos.LargeTextFileIndexerTests
+{
+    public class LargeFileIndexerAccessTests
+    {
+        [Fact]
+        public void Test_Access_Accesses_Stream()
+        {
+            // Arrange
+            true.Should().BeFalse();
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void Test_Access_Accesses_File()
+        {
+            // Arrange
+            true.Should().BeFalse();
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void Test_Access_Has_Count()
+        {
+            // Arrange
+            true.Should().BeFalse();
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void Test_Access_Supports_Linq()
+        {
+            // Arrange
+            true.Should().BeFalse();
+
+            // Act
+
+            // Assert
+        }
+    }
+}
